6540ca3261d9cc9a3f8a00c7ea566a51b391ffff
raquel <raquelanaroman@gmail.com>
2021-11-16 15:10:25 -0500

    init blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "../components/Layout";
+function Blog() {
+    return (
+        <Layout>
+            <div>
+                blog
+            </div>
+        </Layout>
+    )
+}
+
+export default Blog;
