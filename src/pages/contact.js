56aa2448ef6d4402efe399948fd81f639c49a56f
raquel <raquelanaroman@gmail.com>
2021-11-16 15:10:20 -0500

    init contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "../components/Layout";
+function Contact() {
+    return (
+        <Layout>
+            <div>
+                contact
+            </div>
+        </Layout>
+    )
+}
+
+export default Contact;
