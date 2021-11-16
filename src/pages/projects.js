bb2b38a2a35d49cebdee26414cc9d8a9aeec4b86
raquel <raquelanaroman@gmail.com>
2021-11-16 15:10:12 -0500

    init projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "../components/Layout";
+function Projects(){
+    return(
+        <Layout>
+            <div>
+                projects
+            </div>
+        </Layout>
+    )
+}
+
+export default Projects
