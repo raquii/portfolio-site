32426ff60a2c23210cf82202d7974d344b223341
raquel <raquelanaroman@gmail.com>
2021-11-16 15:10:35 -0500

    add links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,16 @@ import React from "react";
 import { Link } from "gatsby";
 
 const Navbar = () => {
-	return <div></div>;
+	return (
+		<nav id="navbar">
+			<Link to="/">Home</Link>
+			<Link to="/about">About</Link>
+			<Link to="/projects">Projects</Link>
+			<Link to="/blog">Blog</Link>
+			<Link to="/contact">Contact</Link>
+			<Link to="/voice">Voice Lessons</Link>
+		</nav>
+	);
 };
 
 export default Navbar;
