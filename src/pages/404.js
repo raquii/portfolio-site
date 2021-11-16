c4165838c1673b20fc639457be7a88e45676574e
raquel <raquelanaroman@gmail.com>
2021-11-16 16:13:55 -0500

    remove default 404 setup

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,45 +1,22 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        It seems I don't have that page on my site.
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <main className="error-page">
+        <section>
+          <h1>404</h1>
+          <p>
+            Oops. Looks like that page doesn't exist.
+            <br />
+            <Link to="/">Go home</Link>
+          </p>
+        </section>
+      </main>
+    </Layout>
   )
 }
 
