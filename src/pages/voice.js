53d6414d1c4e7c4cf7e949daf503e6934d387a0f
raquel <raquelanaroman@gmail.com>
2021-11-16 15:09:42 -0500

    init voice page

---
+++ src/pages/voice.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from "../components/Layout";
+
+function Voice() {
+    return (
+        <Layout>
+            <div>
+                voice
+            </div>
+        </Layout>
+    )
+}
+
+export default Voice;
