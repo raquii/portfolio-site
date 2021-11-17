e108f3ff36d2aa21cbd00731f36fdcdbe71be938
raquel <raquelanaroman@gmail.com>
2021-11-17 11:30:59 -0500

    create contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,22 @@ import Layout from "../components/Layout";
 function Contact() {
     return (
         <Layout>
-            <div>
-                contact
-            </div>
+            <main className="main">
+                <section className="form contact-info">
+                    <h3>Want to get in touch?</h3>
+                </section>
+                <section>
+                    <form className="contact-form">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="email" />
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" />
+                        <button type="submit" className="btn submit-btn">Submit</button>
+                    </form>
+                </section>
+            </main>
         </Layout>
     )
 }
