cd4e8d9166732e5fd44adcc8922695681098fe1e
raquel <raquelanaroman@gmail.com>
2021-11-18 17:52:44 -0500

    add strapi to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,28 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Raquel Roman Rodriguez Portfolio",
+    siteUrl: "https://www.raquelromanrodri.com",
+    title: "Raquel Román-Rodriguez | Full-stack Software Developer",
+    description: "Portfolio site for Raquel Román-Rodriguez, Full-stack Software Developer",
+    author: "Raquel Román-Rodriguez",
   },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 100,
+        collectionTypes: [`jobs`, `projects`],
+      },
+    },
   ],
 };
