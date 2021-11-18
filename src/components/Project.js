241d0f5a627ee8f1f7ae2d5285ae422bbb59f31d
raquel <raquelanaroman@gmail.com>
2021-11-18 17:52:24 -0500

    init project component

---
+++ src/components/Project.js
@@ -0,0 +1,20 @@
+
+import { GatsbyImage } from 'gatsby-plugin-image';
+import React from 'react';
+
+function Project({
+    title,
+    description,
+    github,
+    demo,
+    deploy,
+    image,
+}) {
+    return (
+        <div className="project">
+
+        </div>
+    )
+}
+
+export default Project;
