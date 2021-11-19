7eb72167da4400aa332abbd043b29165f8d272d3
raquel <raquelanaroman@gmail.com>
2021-11-19 14:32:09 -0500

    add projects and query to project pg

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,48 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import Projects from '../components/Projects';
 import Layout from "../components/Layout";
-function Projects() {
-    return (
-        <Layout>
-            <main className="main">
-                <h2 className="pg-header">Projects</h2>
-            </main>
-        </Layout>
-    )
+
+function ProjectsPage({ data }) {
+  const { allStrapiProjects: { nodes: projects } } = data;
+  console.log(projects)
+  return (
+    <Layout>
+      <main className="main">
+        <Projects projects={projects} title="Projects" />
+      </main>
+    </Layout>
+  )
 }
 
-export default Projects
+export const query = graphql`
+  {
+    allStrapiProjects(sort: {fields: id}) {
+      nodes {
+        id
+        title
+        description
+        github
+        demo
+        deployURL
+        stack {
+          id
+          item_name
+        }
+        project_details {
+          id
+          item_name
+        }
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default ProjectsPage;
