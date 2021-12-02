2d1f61b26582091708c3d0ad6fe4e6ed61f6a16a
raquel <raquelanaroman@gmail.com>
2021-12-02 15:22:40 -0500

    move voice pg into nested structure

--- src/pages/voice/index.js
+++ src/pages/voice/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Layout from "../components/Layout";
+import Layout from "../../components/Layout";
 
 function Voice() {
     return (
         <Layout>
             <main className="main">
-                <h2 className="pg-header">Voice Lessons</h2>
+                <h2 className="pg-header">Voice</h2>
             </main>
         </Layout>
     )
