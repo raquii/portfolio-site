9ef7d905086dc90ef980f566fecf79a987ddf461
raquel <raquelanaroman@gmail.com>
2021-12-01 19:27:24 -0500

    reform contact me page layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,21 @@
 import React from 'react';
+
 import Layout from "../components/Layout";
 import SocialLinks from '../constants/socials';
+import ContactForm from '../components/ContactForm';
+
 function Contact() {
+
     return (
         <Layout>
-            <main className="main">
+            <main className="main contact-page">
+                <h1 className="contact-header">Contact Me</h1>
                 <section className="contact-content">
                     <h3>Want to get in touch?</h3>
                     <SocialLinks />
                 </section>
                 <section className="contact-form-container">
-                    <form className="contact-form">
-                        <label htmlFor="name">Name</label>
-                        <input className="form-input" type="text" name="name" id="name" />
-                        <label htmlFor="email">Email</label>
-                        <input className="form-input" type="email" name="email" id="email" />
-                        <label htmlFor="message">Message</label>
-                        <textarea className="form-input" name="message" rows="5" cols="33" id="message" />
-                        <button type="submit" className="btn submit-btn">Submit</button>
-                    </form>
+                    <ContactForm />
                 </section>
             </main>
         </Layout>
