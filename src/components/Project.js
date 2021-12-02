23ea4308d7438d3440e7f344f10532375500d3fd
raquel <raquelanaroman@gmail.com>
2021-12-01 19:26:40 -0500

    reformat project card

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,28 +14,31 @@ function Project({
     id
 }) {
     return (
-        <div className="project">
-            <GatsbyImage
-                alt={title}
-                image={image}
-                className="project-img" />
-            <div className="project-info">
-                <h3>{title}</h3>
-                <p>{description}</p>
-                {/* <ul className="project-details">{details.map(d => <li key={d.id}>→ {d.item_name}</li>)}</ul> */}
-                <div className="project-links">
-                    <a className="project-link" href={github}>
-                        <FaGithub /> GitHub
-                    </a>
-                    {demo && <a className="project-link" href={demo}>
-                        <FaYoutube /> Demo Video
-                    </a>}
-                    {deployURL && <a className="project-link" href={deployURL}>
-                        <FaRocket /> Deployed App
-                    </a>}
-                </div>
-                <div className="project-stack">
-                    {stack.map(item => <span key={item.id} className="project-stack">{item.item_name}</span>)}
+        <div className="project-shell">
+
+            <div className="project">
+                <GatsbyImage
+                    alt={title}
+                    image={image}
+                    className="project-img" />
+                <div className="project-info">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                    {/* <ul className="project-details">{details.map(d => <li key={d.id}>→ {d.item_name}</li>)}</ul> */}
+                    <div className="project-links">
+                        <a className="project-link" href={github}>
+                            <FaGithub /> GitHub
+                        </a>
+                        {demo && <a className="project-link" href={demo}>
+                            <FaYoutube /> Demo Video
+                        </a>}
+                        {deployURL && <a className="project-link" href={deployURL}>
+                            <FaRocket /> Deployed App
+                        </a>}
+                    </div>
+                    <div className="project-stack">
+                        {stack.map(item => <span key={item.id} className="project-stack">{item.item_name}</span>)}
+                    </div>
                 </div>
             </div>
         </div>
