34ab3312250cbc14e75663842a5d1954b44c28a4
raquel <raquelanaroman@gmail.com>
2021-12-02 21:55:45 -0500

    init student portal page

---
+++ src/pages/voice/students.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+function students() {
+    return (
+        <Layout>
+            <main className="main">
+                <h1 className="pg-header">Student Portal</h1>
+                <section>
+
+                </section>
+            </main>
+        </Layout>
+    )
+}
+
+export default students;
