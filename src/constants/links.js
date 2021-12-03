8fc81c479d958a6267663cb0e154020ac4c87e97
raquel <raquelanaroman@gmail.com>
2021-12-02 21:54:13 -0500

    update navbar layout/styling

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
-const data = [
+const links = [
     {
         id: 1,
         text: "home",
@@ -10,34 +11,73 @@ const data = [
     {
         id: 2,
         text: "about",
-        url: "/about/",
+        url: "/about",
     },
     {
         id: 3,
         text: "projects",
-        url: "/projects/",
+        url: "/projects",
     },
     {
         id: 4,
         text: "contact",
-        url: "/contact/",
+        url: "/contact",
+    },
+];
+
+const voiceLinks = [
+    {
+        id: "v1",
+        text: "about",
+        url: "/voice",
     },
     {
-        id: 5,
-        text: "voice",
-        url: "/voice/",
+        id: "v2",
+        text: "lessons",
+        url: "/voice/lessons",
+    },
+    {
+        id: "v3",
+        text: "students",
+        url: "/voice/students",
+    },
+    {
+        id: "v4",
+        text: "media",
+        url: "/voice/media",
     },
 ];
 
 const NavLinks = ({ setShow }) => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <ul className="nav-ul">{data.map(link => {
-            return (
-                <li key={link.id}>
-                    <Link to={link.url} className="nav-link" activeClassName="active-link" onClick={() => setShow(false)}>{link.text}</Link>
-                </li>
-            )
-        })}</ul>
+        <ul className="nav-ul">
+            {links.map(link => {
+                return (
+                    <li key={link.id}>
+                        <Link to={link.url} className="nav-link" activeClassName="active-link" onClick={() => setShow(false)}>{link.text}</Link>
+                    </li>
+                );
+            })}
+            <li className="voice-li" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
+                <div className="voice-link-div nav-link">
+                    <p onClick={() => setOpen(!open)}>voice</p> {open ? <IoIosArrowUp /> : <IoIosArrowDown />}
+
+                </div>
+                <div className={open ? "voice-nav open-popup" : "voice-nav"}>
+                    <ul>
+                        {voiceLinks.map(link => {
+                            return (
+                                <li key={link.id}>
+                                    <Link to={link.url} className="voice-link" activeClassName="active-link" onClick={() => setShow(false)}>{link.text}</Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </li>
+        </ul>
     )
 };
 
