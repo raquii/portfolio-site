e1841933c36b6bd16c24db623d3fdc6d9b0f7eba
raquel <raquelanaroman@gmail.com>
2021-12-03 23:37:43 -0500

    init media pg

---
+++ src/pages/voice/media.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '../../components/Layout';
+
+function media() {
+    return (
+        <Layout>
+            <main className="main">
+                <h1>Media</h1>
+            </main>
+        </Layout>
+    )
+};
+
+export default media;
